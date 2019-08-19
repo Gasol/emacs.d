d42a3f3ee501bab590db0ff6906877ddcebaadeb
Gasol Wu <gasol.wu@gmail.com>
2019-08-19 14:38:07 +0800

    Add MELPA and MELPA-stable repository

--- init.el
+++ init.el
@@ -3,3 +3,8 @@
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)
 (evil-mode 1)
+
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
